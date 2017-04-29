57ac06a316d12154a0b4b74e69e9d9b5c0362d51
Serhiy Stercho <kombo1998@gmail.com>
2017-04-29 16:57:23 +0300

    new constructor

--- SaveButton.cs
+++ SaveButton.cs
@@ -14,14 +14,21 @@ namespace ElectronicCircles
        public List<bool> three = new List<bool>();
        public List<bool> four = new List<bool>();
        public List<bool> five = new List<bool>();
-      
 
-        public SaveButton(bool on,bool tw,bool thr,bool fou,bool fiv){
+       public SaveButton() {
+            one.Clear();
+            two.Clear();
+            three.Clear();
+            four.Clear();
+            five.Clear();
+        }
+
+       public SaveButton(bool on,bool tw,bool thr,bool fou,bool fiv){
             one.Add(on);
             two.Add(tw);
             three.Add(thr);
             four.Add(fou);
             five.Add(fiv);
-        }
+       }
     }
 }
